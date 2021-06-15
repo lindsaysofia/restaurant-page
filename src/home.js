d8e227954a8c109c0e1c2dca911417e34e3abc82
Lindsay Lopez <lopez95@berkeley.edu>
2021-06-14 18:59:05 -0700

    Added restaurant page modules

---
+++ src/home.js
@@ -0,0 +1,6 @@
+
+
+/* <h1>Linda's</h1>
+    <!-- Photo by <a href="https://unsplash.com/@asthetik?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mike Kenneally</a> on <a href="https://unsplash.com/s/photos/coffee?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> -->
+    <img src="../src/image.jpeg" width="300px" alt="An image of a coffee cup with spilled coffee beans">
+    <p>Coffee you'll love.</p> */
